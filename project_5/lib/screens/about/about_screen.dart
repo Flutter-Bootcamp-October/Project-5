8c7cd0686a4809da9ccca1439efd14af93b6a33c
Ruba Hilal <developerruba@outlook.com>
2023-11-13 00:12:38 +0300

    Update about_screen.dart

--- project_5/lib/screens/about/about_screen.dart
+++ project_5/lib/screens/about/about_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AboutScreen extends StatefulWidget {
   const AboutScreen({super.key});
@@ -10,14 +13,30 @@ class AboutScreen extends StatefulWidget {
 //get about data
 
 class _AboutScreenState extends State<AboutScreen> {
+  File? imageGet;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
       children: [
-        //image
+        //image picjer
+        if (imageGet != null)
+          Image.file(
+            imageGet!,
+            width: 85,
+            height: 85,
+          ),
+
         ElevatedButton(
-          onPressed: () {},
+          onPressed: () async {
+            final ImagePicker picker = ImagePicker();
+            XFile? ImageFile =
+                await picker.pickImage(source: ImageSource.gallery);
+            imageGet = File(ImageFile!.path);
+            setState(() {});
+
+            // call api upload image method
+          },
           child: const Text("upload image "),
         ),
         ElevatedButton(
