8e9fa68a94f7114fde9abb22ce77a55f3dd6ae59
Ruba Hilal <developerruba@outlook.com>
2023-11-13 22:18:33 +0300

    update home

--- project_5/lib/screens/home_screen.dart
+++ project_5/lib/screens/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  File? imageGet;
+  // File? imageGet;
   @override
   void initState() {
     // get about data to show and update the screen
@@ -21,8 +21,12 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       // app bar drawer in all pages
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(25),
+        padding: const EdgeInsets.all(15),
         child: Column(
           children: [
             // add refresh library to update
@@ -31,35 +35,62 @@ class _HomeScreenState extends State<HomeScreen> {
 
             Row(
               children: [
-                Column(
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text("Hello NAME! 👋"),
                     Text("ADD CREATED DATE"),
                   ],
                 ),
-                Spacer(),
-                Image.file(
-                  imageGet!.readAsBytes() as File,
-                  width: 85,
-                  height: 85,
+                const Spacer(),
+                // Image.file(
+                //   imageGet?.readAsBytes() as File,
+                //   width: 85,
+                //   height: 85,
+                // ),
+              ],
+            ),
+
+            SizedBox(
+              height: 50,
+            ),
+
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CategoryContainers(
+                  title: 'about me',
+                  backgroundColor: Color.fromARGB(176, 68, 137, 255),
+                ),
+                CategoryContainers(
+                  title: 'Education',
+                  backgroundColor: Color.fromARGB(176, 68, 137, 255),
                 ),
               ],
             ),
-            Container(
-              height: 120,
-              width: 120,
-              decoration: BoxDecoration(
-                  color: Colors.blueAccent.shade200,
-                  borderRadius: BorderRadius.circular(35)),
-              child: const Text("about me"),
+            SizedBox(
+              height: 10,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CategoryContainers(
+                  title: 'skills',
+                  backgroundColor: Color.fromARGB(176, 68, 137, 255),
+                ),
+                CategoryContainers(
+                  title: 'projects',
+                  backgroundColor: Color.fromARGB(176, 68, 137, 255),
+                ),
+              ],
             ),
             // call api upload image method
             ElevatedButton(
               onPressed: () async {
                 final ImagePicker picker = ImagePicker();
-                XFile? ImageFile =
-                    await picker.pickImage(source: ImageSource.gallery);
-                imageGet = File(ImageFile!.path);
+                // XFile? ImageFile =
+                //     await picker.pickImage(source: ImageSource.gallery);
+                // imageGet = File(ImageFile!.path);
                 setState(() {});
               },
               child: const Text("upload image "),
@@ -86,6 +117,31 @@ class _HomeScreenState extends State<HomeScreen> {
              */
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CategoryContainers extends StatelessWidget {
+  const CategoryContainers({
+    super.key,
+    required this.title,
+    required this.backgroundColor,
+  });
+  final String title;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: 175,
+      width: 165,
+      decoration: BoxDecoration(
+          color: backgroundColor, borderRadius: BorderRadius.circular(35)),
+      child: Text(
+        "$title",
+        style: TextStyle(color: Colors.white),
       ),
     );
   }
