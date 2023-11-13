7d57c399ae949b0630330e035db55a8b4834d0a0
Ruba Hilal <developerruba@outlook.com>
2023-11-13 11:30:06 +0300

    edit home screen design

--- project_5/lib/screens/home_screen.dart
+++ project_5/lib/screens/home_screen.dart
@@ -20,55 +20,72 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // show all about data here then navigate to each screen (skills, projects, social, education)
-          // using a container button and make editing and deleting in these screens
-          Image.file(
-            imageGet!.readAsBytes() as File,
-            width: 85,
-            height: 85,
-          ),
-          Container(
-            height: 120,
-            width: 120,
-            decoration: BoxDecoration(
-                color: Colors.blueAccent.shade200,
-                borderRadius: BorderRadius.circular(35)),
-            child: const Text("about me"),
-          ),
-          // call api upload image method
-          ElevatedButton(
-            onPressed: () async {
-              final ImagePicker picker = ImagePicker();
-              XFile? ImageFile =
-                  await picker.pickImage(source: ImageSource.gallery);
-              imageGet = File(ImageFile!.path);
-              setState(() {});
-            },
-            child: const Text("upload image "),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("edit about "),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("delete about "),
-          ),
+      // app bar drawer in all pages
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            // add refresh library to update
+            // show all about data here then navigate to each screen (skills, projects, social, education)
+            // using a container button and make editing and deleting in these screens
 
-          /**
-           * profile image
-           * 
-           * name
-           * title position
-           *
-           * information: phone, location, birthday
-           * about paragraph
-           * 
-           * at the end creat acount date
-           */
-        ],
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Text("Hello NAME! 👋"),
+                    Text("ADD CREATED DATE"),
+                  ],
+                ),
+                Spacer(),
+                Image.file(
+                  imageGet!.readAsBytes() as File,
+                  width: 85,
+                  height: 85,
+                ),
+              ],
+            ),
+            Container(
+              height: 120,
+              width: 120,
+              decoration: BoxDecoration(
+                  color: Colors.blueAccent.shade200,
+                  borderRadius: BorderRadius.circular(35)),
+              child: const Text("about me"),
+            ),
+            // call api upload image method
+            ElevatedButton(
+              onPressed: () async {
+                final ImagePicker picker = ImagePicker();
+                XFile? ImageFile =
+                    await picker.pickImage(source: ImageSource.gallery);
+                imageGet = File(ImageFile!.path);
+                setState(() {});
+              },
+              child: const Text("upload image "),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("edit about "),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("delete about "),
+            ),
+
+            /**
+             * profile image
+             * 
+             * name
+             * title position
+             *
+             * information: phone, location, birthday
+             * about paragraph
+             * 
+             * at the end creat acount date
+             */
+          ],
+        ),
       ),
     );
   }
