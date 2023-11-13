f7bd956a500159b68b3b9852058643adebb23594
Ruba Hilal <developerruba@outlook.com>
2023-11-14 01:28:36 +0300

    add education and project models

---
+++ project_5/lib/models/education_model.dart
@@ -0,0 +1,65 @@
+class Education {
+  Education({
+    required this.msg,
+    required this.data,
+    required this.codeState,
+  });
+  late final String msg;
+  late final Data data;
+  late final int codeState;
+
+  Education.fromJson(Map<String, dynamic> json) {
+    msg = json['msg'];
+    data = Data.fromJson(json['data']);
+    codeState = json['codeState'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final vData = <String, dynamic>{};
+    vData['msg'] = msg;
+    vData['data'] = data.toJson();
+    vData['codeState'] = codeState;
+    return vData;
+  }
+}
+
+class Data {
+  Data({
+    required this.id,
+    required this.userId,
+    required this.graduationDate,
+    required this.university,
+    required this.college,
+    required this.specialization,
+    required this.level,
+  });
+  late final int id;
+  late final int userId;
+  late final String graduationDate;
+  late final String university;
+  late final String college;
+  late final String specialization;
+  late final String level;
+
+  Data.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    userId = json['user_id'];
+    graduationDate = json['graduationDate'];
+    university = json['university'];
+    college = json['college'];
+    specialization = json['specialization'];
+    level = json['level'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final vData = <String, dynamic>{};
+    vData['id'] = id;
+    vData['user_id'] = userId;
+    vData['graduation_date'] = graduationDate;
+    vData['university'] = university;
+    vData['college'] = college;
+    vData['specialization'] = specialization;
+    vData['level'] = level;
+    return vData;
+  }
+}
---
+++ project_5/lib/models/project_model.dart
@@ -0,0 +1,57 @@
+class Project {
+  Project({
+    required this.msg,
+    required this.data,
+    required this.codeState,
+  });
+  late final String msg;
+  late final Data data;
+  late final int codeState;
+
+  Project.fromJson(Map<String, dynamic> json) {
+    msg = json['msg'];
+    data = Data.fromJson(json['data']);
+    codeState = json['codeState'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final vData = <String, dynamic>{};
+    vData['msg'] = msg;
+    vData['data'] = data.toJson();
+    vData['codeState'] = codeState;
+    return vData;
+  }
+}
+
+class Data {
+  Data({
+    required this.id,
+    required this.userId,
+    required this.name,
+    required this.description,
+    required this.state,
+  });
+  late final int id;
+  late final int userId;
+  late final String name;
+  late final String description;
+  late final String state;
+
+  Data.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    userId = json['user_id'];
+    name = json['name'];
+    description = json['description'];
+    state = json['state'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final vData = <String, dynamic>{};
+    vData['id'] = id;
+    vData['user_id'] = userId;
+    vData['name'] = name;
+    vData['description'] = description;
+    vData['state'] = state;
+    return vData;
+  }
+}
